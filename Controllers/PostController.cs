d914501a370d888ab9174648c3610d2171eea62b
Alex Almeida <almeida.alex@outlook.com>
2020-02-14 00:03:52 -0300

    Faltou alguns outros arquivos que esqueci de copiar

---
+++ Controllers/PostController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Blog.Infra;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        IList<Post> _posts = new List<Post>();
+
+
+        public IActionResult Index()
+        {
+            using (var conexao = ConnectionFactory.CriaConexaoAberta())
+            {
+                
+                SqlCommand comando = new SqlCommand("select * from Posts", conexao);
+
+                SqlDataReader leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    var post = new Post();
+                    post.Titulo = leitor["Titulo"].ToString();
+                    post.Resumo = leitor["Resumo"].ToString();
+                    post.Categoria = leitor["categoria"].ToString();
+                    post.Id = Convert.ToInt32(leitor["Id"]);
+
+                    _posts.Add(post);
+                }
+            }
+
+            return View(_posts);
+        }
+
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adiciona(Post post)
+        {
+
+
+            _posts.Add(post);
+            return View("Index", post);
+        }
+
+
+    }
+}
+
+
+
+
+
---
+++ Infra/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Blog.Infra
+{
+    public static class ConnectionFactory
+    {
+        public static SqlConnection CriaConexaoAberta()
+        {
+            var builder = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                          .AddEnvironmentVariables();
+            IConfiguration configuration = builder.Build();
+
+            var conexao = new SqlConnection(configuration.GetConnectionString("Blog"));
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
